6bb6eab1e1d7c8ec8c5e683e6e8f6c4a42638df3
maestroanth <maestroanth@gmail.com>
2017-03-06 23:09:34 -0700

    Add project files.

---
+++ SkillzInDemand-1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SkillzInDemand_1.Startup))]
+namespace SkillzInDemand_1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
