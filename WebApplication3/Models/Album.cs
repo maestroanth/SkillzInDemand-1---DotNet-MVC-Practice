0af5a969fbd74dfc65a3d1abe70f270e767d9bb2
maestroanth <maestroanth@gmail.com>
2017-04-12 19:01:47 -0600

    Added fun app idea to practice dotnet

---
+++ WebApplication3/Models/Album.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Models
+{
+    public class Album
+    {
+        public int ID { get; set; }
+        public string Artist { get; set; }
+        public string AlbumName { get; set; }
+        public string Genre { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
